a4551a671a5bbbb6da612bc61f6eb0338be5d9c6
Bhaskar Joshi <56031616+BhaskarJoshi-01@users.noreply.github.com>
2020-11-05 00:51:00 +0530

    Create ChessEngine.py

---
+++ bhaskar/Chess/ChessEngine.py
@@ -0,0 +1,21 @@
+# This is responsible for storing info and state and determine
+
+class GameState():
+    def __init__(self):
+        # board is a list the first character represent color
+        # second denotes type of character
+        #"--" denotes no piece
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+        self.whiteToMove=True
+        self.moveLog=[]
