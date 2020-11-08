99b3cd4453b4677e233c8536a914bbb20cca057e
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2020-11-08 10:54:21 +0530

    added main() in main.py

--- simple_algos/main.py
+++ simple_algos/main.py
@@ -4,22 +4,26 @@ from chess import BLACK, WHITE
 from algos import color_fav_move, defensive_move, random_next_move, attacking_move, cccp_strategy, alphabetic
 from time import sleep
 
-board = chess.Board()
 
-# seed(0)
-move_count = 0
-print(move_count, ":")
-print(board)
-while not board.is_game_over():
-    if board.turn == chess.WHITE:
-        board.push(alphabetic(board))
-    else:
-        board.push(attacking_move(board))
-    move_count += 1
+def main():
+    # seed(0)
+    board = chess.Board()
+    move_count = 0
     print(move_count, ":")
     print(board)
-    # sleep(0.005)
-    # input()
+    while not board.is_game_over():
+        if board.turn == chess.WHITE:
+            board.push(alphabetic(board))
+        else:
+            board.push(attacking_move(board))
+        move_count += 1
+        print(move_count, ":")
+        print(board)
+        # sleep(0.005)
+        # input()
 
-print(f"\n\nResult: {move_count}")
-print(board.result())
+    print(f"\n\nResult: {move_count}")
+    print(board.result())
+
+if __name__ == '__main__':
+    main()
