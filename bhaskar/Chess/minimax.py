154e0cacc0f862c66af56dd023942c684dc0f282
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2020-11-15 16:07:24 +0530

    odd depth bug fixed

--- bhaskar/Chess/minimax.py
+++ bhaskar/Chess/minimax.py
@@ -13,9 +13,7 @@ def evaluate(board: GameState, for_white: bool) -> int:
         Higher number is in favour of player given.
     """
     eval_stime = time()
-    ret = evaluate_board(board.board)
-    if not for_white:
-        ret *= -1
+    ret = (1 if for_white else -1) * evaluate_board(board.board)
     duration = time()-eval_stime
     global eval_time, evals_cnt
     eval_time += duration
@@ -34,7 +32,7 @@ def minimax(board: GameState, alpha: float, beta: float, maximizer: bool, curDep
         if board.checkMate:
             return (-inf if maximizer else +inf), None
     if curDepth == max_depth:
-        return evaluate(board, board.whiteToMove), None
+        return evaluate(board, not(board.whiteToMove ^ maximizer)), None
 
     # sending inf so that the branch is ignored by parent
     if final_move is not None and time() - stime > timeout:
@@ -91,8 +89,9 @@ def next_move_restricted(board: GameState, max_depth: int) -> Tuple[float, Move]
     evals_cnt = 0
     score, move = minimax(board, alpha=-inf, beta=+inf,
                           maximizer=True, curDepth=0, max_depth=max_depth)
-    print(f"depth {max_depth} done in {time()-depth_stime}.")
-    print(f"evals_time : {eval_time}, eval_cnt: {evals_cnt}, moves_cnt: {moves_cnt}")
+    print(
+        f"depth [{max_depth}] done in {time()-depth_stime} score: {score}"
+        f"evals_time : {eval_time}, eval_cnt: {evals_cnt}, moves_cnt: {moves_cnt}")
     if not move:
         return -inf, None
     return score, move
@@ -105,14 +104,14 @@ def next_move(board: GameState) -> Move:
     global timeout, stime, final_move
     initial_depth = 4
     depth_extension_limit = 10
-    timeout = .5  # in seconds
+    timeout = 20  # in seconds
     final_move = None
     stime = time()
     assert not board.is_game_over()
 
     final_score, final_move = next_move_restricted(
         board, max_depth=initial_depth)
-    print(f"depth [{initial_depth}] chosen", final_score)
+    print(f"depth ({initial_depth}) chosen")
 
     for extension in range(1, depth_extension_limit):
         if time() - stime >= timeout:
@@ -121,6 +120,6 @@ def next_move(board: GameState) -> Move:
             board, max_depth=initial_depth+extension)
         if move is not None and score > final_score:
             final_score, final_move = score, move
-            print(f"depth [{initial_depth+extension}] chosen", final_score)
+            print(f"depth ({initial_depth+extension}) chosen")
 
     return final_move
